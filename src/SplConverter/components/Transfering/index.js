75a8f4de03202c30fa5165dc06d10278a4a6f5c2
BeJoy <firekit13@gmail.com>
2022-02-01 18:55:28 +0300

    fixed disabling and connect render (#48)

--- src/SplConverter/components/Transfering/index.js
+++ src/SplConverter/components/Transfering/index.js
@@ -46,7 +46,7 @@ export const Transfering = () => {
         <a href={`https://etherscan.io/tx/${neonTransferSign}`}
           target='_blank'
           rel='noopener noreferrer'
-          className='text-blue-500'>View on Etherscan</a> : null}
+          className='text-blue-500 mb-4'>View on Etherscan</a> : null}
       {solanaTransferSign ? 
         <a href={`https://solscan.io/tx/${solanaTransferSign}?cluster=devnet`}
           target='_blank'
