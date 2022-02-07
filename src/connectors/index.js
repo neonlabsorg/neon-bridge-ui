33f80083c9285f694287ca8b30cd4724eb84fd92
BeJoy <firekit13@gmail.com>
2022-02-07 15:08:45 +0300

    54 deployments (#55)
    * network deployments created
    
    * fix error text

--- src/connectors/index.js
+++ src/connectors/index.js
@@ -1,4 +1,9 @@
 import { InjectedConnector } from '@web3-react/injected-connector'
+const ids = {
+  'mainnet-beta': 245022934,
+  'testnet': 245022940,
+  'devnet': 245022926
+}
 export const injected = new InjectedConnector({
-  supportedChainIds: [111, 245022926, 245022940, 245022934],
+  supportedChainIds: [ ids[process.env.REACT_APP_NETWORK || 'mainnet-beta'] ]
 })
