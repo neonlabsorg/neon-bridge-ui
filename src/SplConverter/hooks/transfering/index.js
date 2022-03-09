3232c360aa713d32d0d6d5202551a138d29723b1
BeJoy <firekit13@gmail.com>
2022-03-09 15:30:12 +0300

    42 import portal (#67)
    * wip
    
    * restructure portal for public packaging
    
    * migrate on yarn, skip preflight
    
    * add custom conenction from context for transfering module
    
    Co-authored-by: Bejoy <casperyourweb@gmail.com>

--- src/SplConverter/hooks/transfering/index.js
+++ src/SplConverter/hooks/transfering/index.js
@@ -1,17 +1,18 @@
 import { useWallet } from '@solana/wallet-adapter-react'
 import { useWeb3React } from '@web3-react/core'
 import { useStatesContext } from '../../../contexts/states'
-import { useNeonTransfer } from '../../vendor'
+import useNeonTransfer from 'neon-portal/src/react'
 import useTransactionHistory from '../useTransactionHistory'
+import { useConnection } from '../../../contexts/connection'
 
 export const useTransfering = () => {
   const {setPending, setTransfering, rejected, setSolanaTransferSign, setNeonTransferSign, setError} = useStatesContext()
   const {addTransaction} = useTransactionHistory()
+  const connection = useConnection()
   const {publicKey} = useWallet()
   const {account} = useWeb3React()
   const { createNeonTransfer, createSolanaTransfer } = useNeonTransfer({
     onBeforeCreateInstruction: () => {
-      console.log(this)
       setPending(true)
     },
     onBeforeSignTransaction: () => {
@@ -34,7 +35,7 @@ export const useTransfering = () => {
       setTransfering(false)
       setPending(false)
     }
-  })
+  }, connection)
   return { createNeonTransfer, createSolanaTransfer }
 }
 
