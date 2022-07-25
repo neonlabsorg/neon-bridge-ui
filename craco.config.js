ade28cb7a0cf713f80720469a3f261ae5e438bc9
Nazim Gafarov <ng@neonlabs.org>
2022-07-25 11:45:59 +0300

    feat: version file

--- craco.config.js
+++ craco.config.js
@@ -1,16 +1,20 @@
 const path = require("path");
+const VersionFile = require("webpack-version-file");
+
 module.exports = {
   webpack: {
     alias: {
       "@": path.resolve(__dirname, "src/"),
-    }
+    },
+    plugins: [
+      new VersionFile({
+        output: "./build/version.txt",
+      }),
+    ],
   },
   style: {
     postcss: {
-      plugins: [
-        require('tailwindcss'),
-        require('autoprefixer'),
-      ],
+      plugins: [require("tailwindcss"), require("autoprefixer")],
     },
-  }
-}
+  },
+};
