8b9748f0babdebc9f63d6946d9b37da7ba648e6e
Nazim Gafarov <ng@neonlabs.org>
2022-08-05 18:15:10 +0300

    NDEV-483: fix links

--- src/SplConverter/components/IssueReporter/index.js
+++ src/SplConverter/components/IssueReporter/index.js
@@ -1,6 +1,6 @@
 const IssueReporter = ({className = ''}) => {
   return <div className={className}>
-    <a href='https://github.com/neonlabsorg/neon-client-transfer/issues/new'
+    <a href='https://github.com/neonlabsorg/neon-support/issues/new'
       target='_blank' rel='noopener noreferrer' className='text-blue-500 hover:text-blue-600'>
         Report an issue
       </a>
